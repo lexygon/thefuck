1aa04af3c7b0a428e7d4590a79a92046c9bfb5bf
arascan35 <arascan.35@outlook.com>
2020-06-11 14:11:15 +0300

    removed user_dir rules for the sake of using the app internal way

--- thefuck/corrector.py
+++ thefuck/corrector.py
@@ -28,7 +28,7 @@ def get_rules_import_paths():
     # Bundled rules:
     yield Path(__file__).parent.joinpath('rules')
     # Rules defined by user:
-    yield settings.user_dir.joinpath('rules')
+    # yield settings.user_dir.joinpath('rules')
     # Packages with third-party rules:
     for path in sys.path:
         for contrib_module in Path(path).glob('thefuck_contrib_*'):
